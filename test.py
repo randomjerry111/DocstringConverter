1cee8ae7dcbb8ca878af85dbc42e8dbb357bb991
Sean <hehezz@foxmail.com>
2021-02-28 15:06:09 +0800

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+class DoubleLinkList(object):
+    """
+    The definition of double link list.
+    """
+    def __init__(self, head):
+        self.head = head
+
+    def insert_node(self, val, node):
+        
+        pass
+
+    def remove_node(self, val):
+        
+        pass
+
+    def length(self):
+        
+        pass
+
+    def search_node(self, val):
+        
+        pass
